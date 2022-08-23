a6f7d8906e8db73dd4263fb7f3b1e7ff2f522770
PrivetPokaLicoRuka <fop.bulakh.ihor@gmail.com>
2022-08-23 22:29:56 +0300

    byte code

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,14 @@
-public class Program {
+class Program {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        //System.out.println("Hello Java!");
+        byte a = 0;
+    }
+    private static void printBinary(byte value, int size){
+        for(int i = size - 1; i >= 0; i--){
+            long mask = 1 << i;
+            long result = (mask & value) >> i;
+            System.out.print(result);
+        }
+        System.out.println();
     }
 }
