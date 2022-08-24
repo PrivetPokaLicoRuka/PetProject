327488edfe8ca6cb0a57b577a801700e901235b6
PrivetPokaLicoRuka <fop.bulakh.ihor@gmail.com>
2022-08-24 13:31:14 +0300

    printBinary

--- src/Program.java
+++ src/Program.java
@@ -1,11 +1,12 @@
 class Program {
     public static void main(String[] args) {
         //System.out.println("Hello Java!");
-        byte a = 0;
+    byte a = 8;
+    printBinary(a,8);
     }
-    private static void printBinary(byte value, int size){
+    private static void printBinary(int value, int size){
         for(int i = size - 1; i >= 0; i--){
-            long mask = 1 << i;
+            long mask = 1L << i;
             long result = (mask & value) >> i;
             System.out.print(result);
         }
